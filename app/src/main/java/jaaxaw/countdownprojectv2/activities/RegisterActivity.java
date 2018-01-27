989b055fef349b6611d18c1c43930a7d689ccb46
BLANDIN Florian <florianblandin-iut@outlook.fr>
2018-01-27 11:29:21 +0100

    Login and Register works

--- app/src/main/java/jaaxaw/countdownprojectv2/activities/RegisterActivity.java
+++ app/src/main/java/jaaxaw/countdownprojectv2/activities/RegisterActivity.java
@@ -2,23 +2,37 @@ package jaaxaw.countdownprojectv2.activities;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import jaaxaw.countdownprojectv2.R;
 import jaaxaw.countdownprojectv2.sql.DatabaseHelper;
+
+import static android.R.attr.data;
+import static com.facebook.share.internal.DeviceShareDialogFragment.TAG;
 
 public class RegisterActivity extends Activity{
 
     private EditText _lastname, _firstname, _username, _email, _password;
     private Button _register_button;
+    private LoginButton loginButton;
     private SQLiteDatabase db;
     private SQLiteOpenHelper openHelper;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,29 @@ public class RegisterActivity extends Activity{
         _password = (EditText) findViewById(R.id.password);
         _register_button = (Button) findViewById(R.id.register_btn);
 
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("fb_connexion", "facebook:onSuccess:" + loginResult);
+                //handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("fb_connexion", "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("fb_connexion", "facebook:onError", error);
+                // ...
+            }
+        });
+
         _register_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -46,6 +83,15 @@ public class RegisterActivity extends Activity{
                 Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void insertDatas(String lastname, String firstname, String username, String email, String password){
